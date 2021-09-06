f5c2a62a01db4110fa5c35844341e8252efa3cd6
Kirmmin <kirmmin@hotmail.com>
2021-09-06 07:08:40 -0700

    Player/Create: Fix Starting Location positions & rotations

--- Source/NexusForever.Database.Character/Migrations/20210906140425_StartingLocations.cs
+++ Source/NexusForever.Database.Character/Migrations/20210906140425_StartingLocations.cs
@@ -32,17 +32,17 @@ namespace NexusForever.Database.Character.Migrations
                 columns: new[] { "creationStart", "faction", "race", "comment", "rx", "worldId", "x", "y", "z" },
                 values: new object[,]
                 {
-                    { (byte)4, (ushort)167, (byte)1, "Exile Human - Novice", -157.6469f, 3460u, 29.1286f, -853.8716f, -560.188f },
-                    { (byte)4, (ushort)166, (byte)12, "Dominion Mechari - Novice", -157.6469f, 3460u, 29.1286f, -853.8716f, -560.188f },
-                    { (byte)5, (ushort)166, (byte)1, "Dominion Cassian - Level 50", -43.72939f, 22u, -3343.58f, -887.4646f, -536.03f },
-                    { (byte)3, (ushort)166, (byte)1, "Dominion Cassian - Veteran", -27.29293f, 1387u, -3835.341f, -980.2174f, -6050.524f },
-                    { (byte)4, (ushort)166, (byte)1, "Dominion Cassian - Novice", -157.6469f, 3460u, 29.1286f, -853.8716f, -560.188f },
-                    { (byte)5, (ushort)166, (byte)5, "Dominion Draken - Level 50", -43.72939f, 22u, -3343.58f, -887.4646f, -536.03f },
-                    { (byte)3, (ushort)166, (byte)5, "Dominion Draken - Veteran", -93.37776f, 870u, -8260.709f, -995.077f, -239.0409f },
-                    { (byte)4, (ushort)166, (byte)5, "Dominion Draken - Novice", -157.6469f, 3460u, 29.1286f, -853.8716f, -560.188f },
-                    { (byte)5, (ushort)166, (byte)13, "Dominion Chua - Level 50", -43.72939f, 22u, -3343.58f, -887.4646f, -536.03f },
-                    { (byte)3, (ushort)166, (byte)13, "Dominion Chua - Veteran", -93.37776f, 870u, -8260.709f, -995.077f, -239.0409f },
-                    { (byte)4, (ushort)166, (byte)13, "Dominion Chua - Novice", -157.6469f, 3460u, 29.1286f, -853.8716f, -560.188f }
+                    { (byte)4, (ushort)167, (byte)1, "Exile Human - Novice", -2.751458f, 3460u, 29.1286f, -853.8716f, -560.188f },
+                    { (byte)4, (ushort)166, (byte)12, "Dominion Mechari - Novice", -2.751458f, 3460u, 29.1286f, -853.8716f, -560.188f },
+                    { (byte)5, (ushort)166, (byte)1, "Dominion Cassian - Level 50", -0.7632219f, 22u, -3343.58f, -887.4646f, -536.03f },
+                    { (byte)3, (ushort)166, (byte)1, "Dominion Cassian - Veteran", -0.45682f, 1387u, -3835.341f, -980.2174f, -6050.524f },
+                    { (byte)4, (ushort)166, (byte)1, "Dominion Cassian - Novice", -2.751458f, 3460u, 29.1286f, -853.8716f, -560.188f },
+                    { (byte)5, (ushort)166, (byte)5, "Dominion Draken - Level 50", -0.7632219f, 22u, -3343.58f, -887.4646f, -536.03f },
+                    { (byte)3, (ushort)166, (byte)5, "Dominion Draken - Veteran", -2.215535f, 870u, -8261.398f, -995.471f, -242.3648f },
+                    { (byte)4, (ushort)166, (byte)5, "Dominion Draken - Novice", -2.751458f, 3460u, 29.1286f, -853.8716f, -560.188f },
+                    { (byte)5, (ushort)166, (byte)13, "Dominion Chua - Level 50", -0.7632219f, 22u, -3343.58f, -887.4646f, -536.03f },
+                    { (byte)3, (ushort)166, (byte)13, "Dominion Chua - Veteran", -2.215535f, 870u, -8261.398f, -995.471f, -242.3648f },
+                    { (byte)4, (ushort)166, (byte)13, "Dominion Chua - Novice", -2.751458f, 3460u, 29.1286f, -853.8716f, -560.188f }
                 });
 
             migrationBuilder.InsertData(
@@ -55,8 +55,8 @@ namespace NexusForever.Database.Character.Migrations
                 columns: new[] { "creationStart", "faction", "race", "comment", "rx", "worldId", "x", "y", "z" },
                 values: new object[,]
                 {
-                    { (byte)3, (ushort)167, (byte)16, "Exile Mordesh - Veteran", -98.98854f, 990u, -771.823f, -904.2852f, -2269.56f },
-                    { (byte)4, (ushort)167, (byte)16, "Exile Mordesh - Novice", -157.6469f, 3460u, 29.1286f, -853.8716f, -560.188f }
+                    { (byte)3, (ushort)167, (byte)16, "Exile Mordesh - Veteran", -1.1214035f, 990u, -771.823f, -904.2852f, -2269.56f },
+                    { (byte)4, (ushort)167, (byte)16, "Exile Mordesh - Novice", -2.751458f, 3460u, 29.1286f, -853.8716f, -560.188f }
                 });
 
             migrationBuilder.InsertData(
@@ -69,8 +69,8 @@ namespace NexusForever.Database.Character.Migrations
                 columns: new[] { "creationStart", "faction", "race", "comment", "rx", "worldId", "x", "y", "z" },
                 values: new object[,]
                 {
-                    { (byte)3, (ushort)167, (byte)4, "Exile Aurin - Veteran", -98.98854f, 990u, -771.823f, -904.2852f, -2269.56f },
-                    { (byte)4, (ushort)167, (byte)4, "Exile Aurin - Novice", -157.6469f, 3460u, 29.1286f, -853.8716f, -560.188f }
+                    { (byte)3, (ushort)167, (byte)4, "Exile Aurin - Veteran", -1.1214035f, 990u, -771.823f, -904.2852f, -2269.56f },
+                    { (byte)4, (ushort)167, (byte)4, "Exile Aurin - Novice", -2.751458f, 3460u, 29.1286f, -853.8716f, -560.188f }
                 });
 
             migrationBuilder.InsertData(
@@ -83,8 +83,8 @@ namespace NexusForever.Database.Character.Migrations
                 columns: new[] { "creationStart", "faction", "race", "comment", "rx", "worldId", "x", "y", "z" },
                 values: new object[,]
                 {
-                    { (byte)3, (ushort)167, (byte)3, "Exile Granok- Veteran", 19.33001f, 426u, 4110.71f, -658.6249f, -5145.48f },
-                    { (byte)4, (ushort)167, (byte)3, "Exile Granok - Novice", -157.6469f, 3460u, 29.1286f, -853.8716f, -560.188f }
+                    { (byte)3, (ushort)167, (byte)3, "Exile Granok- Veteran", 0.317613f, 426u, 4110.71f, -658.6249f, -5145.48f },
+                    { (byte)4, (ushort)167, (byte)3, "Exile Granok - Novice", -2.751458f, 3460u, 29.1286f, -853.8716f, -560.188f }
                 });
 
             migrationBuilder.InsertData(
@@ -97,9 +97,9 @@ namespace NexusForever.Database.Character.Migrations
                 columns: new[] { "creationStart", "faction", "race", "comment", "rx", "worldId", "x", "y", "z" },
                 values: new object[,]
                 {
-                    { (byte)3, (ushort)167, (byte)1, "Exile Human - Veteran", 19.33001f, 426u, 4110.71f, -658.6249f, -5145.48f },
-                    { (byte)3, (ushort)166, (byte)12, "Dominion Mechari - Veteran", -27.29293f, 1387u, -3835.341f, -980.2174f, -6050.524f },
-                    { (byte)5, (ushort)166, (byte)12, "Dominion Mechari - Level 50", -43.72939f, 22u, -3343.58f, -887.4646f, -536.03f }
+                    { (byte)3, (ushort)167, (byte)1, "Exile Human - Veteran", 0.317613f, 426u, 4110.71f, -658.6249f, -5145.48f },
+                    { (byte)3, (ushort)166, (byte)12, "Dominion Mechari - Veteran", -0.45682f, 1387u, -3835.341f, -980.2174f, -6050.524f },
+                    { (byte)5, (ushort)166, (byte)12, "Dominion Mechari - Level 50", -0.7632219f, 22u, -3343.58f, -887.4646f, -536.03f }
                 });
         }
 
