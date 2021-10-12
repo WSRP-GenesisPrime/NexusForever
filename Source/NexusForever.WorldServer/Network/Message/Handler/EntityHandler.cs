0903b6ab5c021637a15985043ae9a9a0fda2faf9
Dekker3D <dekker3d@gmail.com>
2021-10-12 19:52:10 +0200

    Mounted-logout exception fix suggested by Rawaho.

--- Source/NexusForever.WorldServer/Network/Message/Handler/EntityHandler.cs
+++ Source/NexusForever.WorldServer/Network/Message/Handler/EntityHandler.cs
@@ -22,6 +22,10 @@ namespace NexusForever.WorldServer.Network.Message.Handler
         public static void HandleEntityCommand(WorldSession session, ClientEntityCommand entityCommand)
         {
             WorldEntity mover = session.Player;
+
+            if (mover == null)
+                return;
+
             if (session.Player.ControlGuid != session.Player.Guid)
                 mover = session.Player.GetVisible<WorldEntity>(session.Player.ControlGuid);
 
