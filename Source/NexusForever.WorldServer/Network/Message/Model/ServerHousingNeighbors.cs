1271112cf64107b0af1fb37e2d1a11e3e6c9d7f9
Dekker3D <dekker3d@gmail.com>
2021-11-22 17:24:12 +0100

    Filled in ServerHousingNeighbors packet.

--- Source/NexusForever.WorldServer/Network/Message/Model/ServerHousingNeighbors.cs
+++ Source/NexusForever.WorldServer/Network/Message/Model/ServerHousingNeighbors.cs
@@ -1,14 +1,38 @@
 using NexusForever.Shared.Network;
 using NexusForever.Shared.Network.Message;
+using System.Collections.Generic;
 
 namespace NexusForever.WorldServer.Network.Message.Model
 {
     [Message(GameMessageOpcode.ServerHousingNeighbors)]
     public class ServerHousingNeighbors : IWritable
     {
+        public class NeighborData : IWritable
+        {
+            public ulong ContactID1 { get; set; }
+            public ulong ContactID2 { get; set; }
+            public ushort Unknown1 { get; set; } = 0;
+            public ulong CharacterID { get; set; }
+            public uint Unknown2 { get; set; } = 8;
+
+            public void Write(GamePacketWriter writer)
+            {
+                writer.Write(ContactID1);
+                writer.Write(ContactID2);
+                writer.Write(Unknown1);
+                writer.Write(CharacterID);
+                writer.Write(Unknown2); // can be 3 or 4 bytes when its value is 0; otherwise always 4 bytes.
+            }
+        }
+
+        public List<NeighborData> Neighbors { get; set; } = new List<NeighborData>();
         public void Write(GamePacketWriter writer)
         {
-            writer.Write(0);
+            writer.Write((uint) Neighbors.Count);
+            foreach(var neighbor in Neighbors)
+            {
+                neighbor.Write(writer);
+            }
         }
     }
 }
