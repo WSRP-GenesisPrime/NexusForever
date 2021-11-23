138073130684c293752467e95604738628d2e1f8
Kirmmin <kirmmin@hotmail.com>
2021-11-22 22:10:58 -0800

    [Scripts/Quests] Crimson Isle quests (1/??)

---
+++ Source/NexusForever.WorldServer/Script/Quests/CrimsonIsle/Q5573-PoweringDown.cs
@@ -0,0 +1,25 @@
+﻿using NexusForever.WorldServer.Game.Entity;
+using NexusForever.WorldServer.Game.Quest;
+using NexusForever.WorldServer.Game.Quest.Static;
+
+namespace NexusForever.WorldServer.Script.Quests.CrimsonIsle
+{
+    [Script(5573)]
+    public class Q5573_PoweringDown : QuestScript
+    {
+        static uint QOBJ_POWER_REGULATORS = 8229;
+        static uint QOBJ_CINEMATIC_COMPLETE = 12870;
+
+        public override void OnObjectiveUpdate(Player player, Quest quest, QuestObjective objective)
+        {
+            if (objective.ObjectiveInfo.Id != QOBJ_POWER_REGULATORS)
+                return;
+
+            if (objective.IsComplete() && quest.State != QuestState.Achieved)
+            {
+                // Play Cinematic
+                player.QuestManager.ObjectiveUpdate(QOBJ_CINEMATIC_COMPLETE, 1u);
+            }
+        }
+    }
+}
