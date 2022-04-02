ad11eee465b69af7b2bba3279cdddb098b1d1389
Dekker3D <dekker3d@gmail.com>
2022-04-02 20:13:53 +0200

    Message stubs for addon comms

---
+++ Source/NexusForever.WorldServer/Network/Message/Model/ClientIcCommSendMessage.cs
@@ -0,0 +1,16 @@
+﻿using NexusForever.Shared.Network;
+using NexusForever.Shared.Network.Message;
+using NexusForever.WorldServer.Network.Message.Model.Shared;
+
+namespace NexusForever.WorldServer.Network.Message.Model
+{
+    [Message(GameMessageOpcode.ClientIcCommSendMessage)]
+    public class ClientIcCommSendMessage : IReadable
+    {
+
+        public void Read(GamePacketReader reader)
+        {
+            string ChannelName   = reader.ReadWideString();
+        }
+    }
+}
