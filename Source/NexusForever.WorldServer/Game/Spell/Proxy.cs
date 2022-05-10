a7c07b62907d0368b4b68a71c4afda7d6cade965
Matt-Hurd <sabahbob@gmail.com>
2022-05-10 18:46:29 -0500

    [Spells/Fix] Properly tick proxies

--- Source/NexusForever.WorldServer/Game/Spell/Proxy.cs
+++ Source/NexusForever.WorldServer/Game/Spell/Proxy.cs
@@ -64,7 +64,7 @@ namespace NexusForever.WorldServer.Game.Spell
                     double tickTime = Entry.TickTime;
                     if (Entry.DurationTime > 0)
                     {
-                        for (int i = 1; i >= Entry.DurationTime / tickTime; i++)
+                        for (int i = 1; i <= Entry.DurationTime / tickTime; i++)
                             events.EnqueueEvent(new SpellEvent(tickTime * i / 1000d, () =>
                             {
                                 caster.CastSpell(Entry.DataBits01, proxyParameters);
