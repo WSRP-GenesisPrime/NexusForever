dae65eef1032cde9362d393b1f70ad4f8400628c
Dekker3D <dekker3d@gmail.com>
2022-05-28 19:12:58 +0200

    Database migration to include new HookBag and HookBagIndex values. Still no clue what they do.

---
+++ Source/NexusForever.Database.Character/Migrations/20220528164103_DecorEntities.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NexusForever.Database.Character.Migrations
+{
+    public partial class DecorEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<uint>(
+                name: "hookBagIndex",
+                table: "residence_decor",
+                type: "int(10) unsigned",
+                nullable: false,
+                defaultValue: 0u);
+
+            migrationBuilder.AddColumn<uint>(
+                name: "hookIndex",
+                table: "residence_decor",
+                type: "int(10) unsigned",
+                nullable: false,
+                defaultValue: 0u);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "hookBagIndex",
+                table: "residence_decor");
+
+            migrationBuilder.DropColumn(
+                name: "hookIndex",
+                table: "residence_decor");
+        }
+    }
+}
