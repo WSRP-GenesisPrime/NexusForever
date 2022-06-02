9204cf59cfe0d5a771416b3e680cf3fe21e139bd
Dekker3D <dekker3d@gmail.com>
2022-06-02 21:19:35 +0200

    Toggle-able decor items are now mapped to the relevant script in a simpler way, and AMPs are disabled to prevent players from disabling their characters with them.

--- Source/NexusForever.WorldServer/Game/Entity/Simple.cs
+++ Source/NexusForever.WorldServer/Game/Entity/Simple.cs
@@ -48,36 +48,14 @@ namespace NexusForever.WorldServer.Game.Entity
 
         private void SetScript()
         {
-            switch(this.CreatureId)
+            Creature2Entry creature = GameTableManager.Instance.Creature2.GetEntry(this.CreatureId);
+            if(creature == null)
             {
-                case 26348: // Housing - Decor - Lighting - Candle (Medium)
-                case 59484: // Housing - Decor - Lighting - Candle (Tall)
-                case 59485: // Housing - Decor - Lighting - Candle (Short)
-                case 59487: // Housing - Decor - Lighting - Hanging Lamp (Aurin)
-                case 59488: // Housing - Decor - Lighting - Yellow Sconce (Aurin)
-                case 59489: // Housing - Decor - Lighting - Cage Light
-                //case 59490: // Housing - Decor - Lighting - Lamp (Hanging, Draken) // does nothing?
-                case 59491: // Housing - Decor - Lighting - Skull Candle Holder (Draken)
-                case 59492: // Housing - Decor - Lighting - Hanging Spotlight
-                case 59493: // Housing - Decor - Lighting - Robot Lamp (Marauder)
-                case 59494: // Housing - Decor - Lighting - Glass Sconce (Orange)
-                //case 72386: // Housing - Decor - Lighting - Frostforged Brazier // does nothing?
-                case 72845: // Housing - Decor - Lighting - Frostforged Watchfire
-                //case 75339: // Housing - Decor - Lighting - Wall Sconce (Redmoon) // does nothing?
-                    script = new SimpleStateScript(this, StandState.State0, StandState.State1);
-                    break;
-                case 59495: // Housing - Decor - Lighting - Ritual Candle Circle (Light source, but the states are inverted)
-                case 59496: // Housing - Decor - Fencing - Fence Arch (Picket)
-                case 65852: // Housing - Decor - Doors - Gothic Gate
-                case 70052: // Housing - Decor - Activated - Door
-                case 72357: // Housing - Decor - Lighting - Osun Battle-Lantern
-                case 72358: // Housing - Decor - Lighting - Frostglow Lantern
-                case 72359: // Housing - Decor - Lighting - Icefrost Candles
-                case 72360: // Housing - Decor - Lighting - Frostforged Candles
-                case 73554: // Housing - Decor - Lighting - Mechari Lamp
-                case 75398: // Redmoon Door (Circular) - Decor - Housing Active Prop
-                    script = new SimpleStateScript(this, StandState.State1, StandState.State0);
-                    break;
+                return;
+            }
+            if(creature.Spell4IdActivate00 == 1817) // lights and doors, plus a lot of other decor.
+            {
+                script = new SimpleStateScript(this, StandState.State1, StandState.State0);
             }
         }
 
@@ -160,11 +138,17 @@ namespace NexusForever.WorldServer.Game.Entity
 
     public abstract class EntityScript
     {
-        public abstract void OnActivateCast(Player activator);
+        public virtual void OnActivateCast(Player activator)
+        {
+        }
 
-        public abstract void OnActivate(Player activator);
+        public virtual void OnActivate(Player activator)
+        {
+        }
 
-        public abstract void OnVisible(GridEntity entity);
+        public virtual void OnVisible(GridEntity entity)
+        {
+        }
     }
 
     public class SimpleStateScript : EntityScript
@@ -181,11 +165,6 @@ namespace NexusForever.WorldServer.Game.Entity
         private StandState state1;
         private StandState state2;
 
-        public override void OnActivate(Player activator)
-        {
-            
-        }
-
         public override void OnActivateCast(Player activator)
         {
             open = !open;
@@ -200,7 +179,7 @@ namespace NexusForever.WorldServer.Game.Entity
 
         public override void OnVisible(GridEntity entity)
         {
-            if(entity is Player player)
+            if (entity is Player player)
             {
                 player.Session.EnqueueMessageEncrypted(new ServerEmote()
                 {
