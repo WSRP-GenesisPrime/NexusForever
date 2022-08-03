355587e5aa1f27011a58ae64892682dd70bf7a46
Kirmmin <kirmmin@hotmail.com>
2022-08-03 13:35:18 -0700

    [Player/Fix] Fixed packet to no longer emit from target. This was necessary because if the target emitted the packet too early, the requesting client would forget about it.

--- Source/NexusForever.WorldServer/Game/Entity/Player.cs
+++ Source/NexusForever.WorldServer/Game/Entity/Player.cs
@@ -734,8 +734,14 @@ namespace NexusForever.WorldServer.Game.Entity
             base.AddVisible(entity);
             Session.EnqueueMessageEncrypted(((WorldEntity)entity).BuildCreatePacket());
 
-            if (entity is Player player)
-                player.PathManager.SendSetUnitPathTypePacket();
+            if (entity is Player playerEntity)
+            {
+                Session.EnqueueMessageEncrypted(new ServerSetUnitPathType
+                {
+                    Guid = playerEntity.Guid,
+                    Path = playerEntity.Path
+                });
+            }
             else
                 Session.EnqueueMessageEncrypted(new Server08B3
                 {
