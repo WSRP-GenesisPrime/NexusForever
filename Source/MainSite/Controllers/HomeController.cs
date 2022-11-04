61efb67f1fd4cbb36fd7c7e95fec3e1c705f3c67
Dekker3D <dekker3d@gmail.com>
2022-11-04 07:40:18 +0100

    Add registration page.

---
+++ Source/MainSite/Controllers/HomeController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MainSite.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Net.Sockets;
+using System.Collections.Immutable;
+using NexusForever.Database.Auth;
+using NexusForever.Shared.Cryptography;
+using NexusForever.Shared.Database;
+using NexusForever.Database.Auth.Model;
+using NexusForever.Shared.Configuration;
+
+namespace MainSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private bool isOnline = false;
+        private ImmutableList<ServerModel> servers;
+
+        public IActionResult Index()
+        {
+            GetStatusImage();
+            return View();
+        }
+        
+
+        
+        public IActionResult Register(AccountBaseModel newUser)
+        {
+            string username = newUser.Email?.ToLower();
+            GetStatusImage();
+            if (username != null && newUser.Confirmation != null && newUser.Password != null)
+            {
+                if (newUser.Password.Equals(newUser.Confirmation))
+                {
+                    try
+                    {
+                        (string salt, string verifier) = PasswordProvider.GenerateSaltAndVerifier(username, newUser.Password);
+                        DatabaseManager.Instance.AuthDatabase.CreateAccount(username, salt, verifier, 1);
+                        return View("RegisterSuccess");
+                    }
+                    catch (Exception)
+                    {
+                        return View("DBException");
+                    }
+                }
+                return View("RegisterFailed");
+
+            }
+            return View("Index");
+        }
+
+        private void GetStatusImage()
+        {
+            try
+            {
+                if (servers == null)
+                {
+                    servers = GetServers();
+                }
+
+                isOnline = PingHost(servers.First().Host, servers.First().Port);
+
+                switch (isOnline)
+                {
+                    case true:
+                        ViewBag.StatusSrc = "statusOn.png";
+                        break;
+                    default:
+                        ViewBag.StatusSrc = "statusOff.png";
+                        break;
+
+                }
+            }
+            catch
+            {
+                ViewBag.StatusSrc = "statusOff.png";
+            }
+        }
+
+        private static bool PingHost(string hostIP, int portNr)
+        {
+            try
+            {
+                using (var client = new TcpClient(hostIP, portNr))
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private static ImmutableList<ServerModel> GetServers()
+        {
+            return DatabaseManager.Instance.AuthDatabase.GetServers();
+        }
+    }
+}
---
+++ Source/MainSite/Startup.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NexusForever.Shared.Configuration;
+using NexusForever.Shared.Database;
+
+namespace MainSite
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            ConfigurationManager<AuthServerConfiguration>.Instance.Initialise("AuthServer.json");
+            DatabaseManager.Instance.Initialise(ConfigurationManager<AuthServerConfiguration>.Instance.Config.Database);
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => false;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseRouting();
+            app.UseEndpoints(ep =>
+            {
+                ep.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
