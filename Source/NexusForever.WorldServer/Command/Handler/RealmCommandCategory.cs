2b4c206fe91d37701f4aabc0f67bb1950300f906
scr1ptkitty <eharrisgames@gmail.com>
2023-01-21 12:21:18 -0500

    Replace update command with aggro switch command
    Didn't see much use in a command that can update so many realm config values, which would require reloading every grid anyway.

--- Source/NexusForever.WorldServer/Command/Handler/RealmCommandCategory.cs
+++ Source/NexusForever.WorldServer/Command/Handler/RealmCommandCategory.cs
@@ -93,72 +93,35 @@ namespace NexusForever.WorldServer.Command.Handler
             context.SendMessage($"Currently up for {WorldServer.Uptime:%d}d {WorldServer.Uptime:%h}h {WorldServer.Uptime:%m}m {WorldServer.Uptime:%s}s");
         }
 
-        [Command(Permission.Realm, "A collection of commands to manage the realm config.", "config")]
-        public class RealmConfigCommandCategory : CommandCategory
+        [Command(Permission.Realm, "Re-initialize WorldServer config values from the configuration file.", "reload")]
+        public void HandleWorldConfigReload(ICommandContext context)
         {
-            [Command(Permission.Realm, "Re-initialize WorldServer config values from the configuration file.", "reload")]
-            public void HandleWorldConfigReload(ICommandContext context)
+            ConfigurationManager<WorldServerConfiguration>.Instance.Initialise("WorldServer.json");
+            context.SendMessage($"WorldServer configuration re-initialized.");
+        }
+
+        // (GENESIS PRIME) Aggro Switch enabled/disabled in WorldServer config
+        [Command(Permission.Realm, "Change the aggro switch enablement status.", "aggro")]
+        public void HandleWorldConfigUpdate(ICommandContext context,
+            [Parameter("New value for the aggro switch.")]
+            string value)
+        {
+            WorldServerConfiguration worldServerConfiguration = ConfigurationManager<WorldServerConfiguration>.Instance.Config;
+            WorldServerConfiguration.MapConfig worldServerMapConfig = worldServerConfiguration.Map;
+
+            string newConfig = value.ToLower();
+            if (value.Equals("on"))
+                newConfig = "true";
+            else if (value.Equals("off"))
+                newConfig = "false";
+            else
             {
-                ConfigurationManager<WorldServerConfiguration>.Instance.Initialise("WorldServer.json");
-                context.SendMessage($"WorldServer configuration re-initialized.");
+                context.SendError($"Invalid input value: {value}");
+                return;
             }
 
-            [Command(Permission.Realm, "Dynamically update the WorldServer configuration. These changes will be lost on server restart.", "update")]
-            public void HandleWorldConfigUpdate(ICommandContext context,
-                [Parameter("Name of the configuration field.")]
-                string name,
-                [Parameter("New value for the configuration field.")]
-                string value)
-            {
-                WorldServerConfiguration worldServerConfiguration = ConfigurationManager<WorldServerConfiguration>.Instance.Config;
-                WorldServerConfiguration.MapConfig worldServerMapConfig = worldServerConfiguration.Map;
-                if ("GridActionThreshold" == name)
-                {
-                    worldServerMapConfig.GridActionThreshold = UInt32.Parse(value);
-                }
-                else if ("GridActionMaxRetry" == name)
-                {
-                    worldServerMapConfig.GridActionMaxRetry = UInt32.Parse(value);
-                }
-                else if ("GridUnloadTimer" == name)
-                {
-                    worldServerMapConfig.GridUnloadTimer = Double.Parse(value);
-                }
-                else if ("MaxInstances" == name)
-                {
-                    worldServerMapConfig.MaxInstances = UInt32.Parse(value);
-                }
-                else if ("MessageOfTheDay" == name || "motd" == name)
-                {
-                    worldServerConfiguration.MessageOfTheDay = value;
-                    WorldServer.RealmMotd = worldServerConfiguration.MessageOfTheDay;
-                    foreach (WorldSession session in NetworkManager<WorldSession>.Instance)
-                        GlobalChatManager.Instance.SendMessage(session, WorldServer.RealmMotd, "MOTD", ChatChannelType.Realm);
-                }
-                else if ("LengthOfInGameDay" == name)
-                {
-                    worldServerConfiguration.LengthOfInGameDay = UInt32.Parse(value);
-                }
-                else if ("CrossFactionChat" == name)
-                {
-                    worldServerConfiguration.CrossFactionChat = Boolean.Parse(value);
-                }
-                else if ("MaxPlayers" == name || "max" == name)
-                {
-                    worldServerConfiguration.MaxPlayers = UInt32.Parse(value);
-                    LoginQueueManager.Instance.SetMaxPlayers(worldServerConfiguration.MaxPlayers);
-                }
-                // (GENESIS PRIME) Aggro Switch enabled/disabled in WorldServer config
-                else if ("AggroSwitchEnabled" == name)
-                {
-                    worldServerConfiguration.AggroSwitchEnabled = Boolean.Parse(value);
-                }
-                else
-                {
-                    context.SendError($"Dynamic configuration update not supported for: {name}");
-                }
-                context.SendMessage($"WorldServer configuration updated: {name} = {value}");
-            }
+            worldServerConfiguration.AggroSwitchEnabled = Boolean.Parse(newConfig);
+            context.SendMessage($"AggroSwitch configuration updated: {newConfig}");
         }
     }
 }
